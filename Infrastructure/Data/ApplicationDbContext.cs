287f2256825f5e8a9fdbee3f35ee279b936ad1ae
Mahesh Singh Madai <maheshmadai977@gmail.com>
2023-04-06 15:02:11 +0545

    Project Setup

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _db.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var products = await _db.Products.FindAsync(id);
+
+            return Ok(products);
+        }
+    }
+}
