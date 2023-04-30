701f953e4815e00fece8cc29f92bb60554885bee
Mahesh Singh Madai <maheshmadai977@gmail.com>
2023-04-30 11:03:12 +0545

    Search functionality added

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -29,8 +29,9 @@ export class ShopService {
    
       params = params.append('pageIndex', shopParams.pageNumber.toString());
       params = params.append('pageSize', shopParams.pageSize.toString());
+      if (shopParams.search) params = params.append('search', shopParams.search);
       
-    return this.Http.get<IPagination>(this.baseUrl + 'Product?pageSize=50',{observe:'response',params})
+    return this.Http.get<IPagination>(this.baseUrl + 'Product',{observe:'response',params})
       .pipe(
         map(response => {
           return response.body
