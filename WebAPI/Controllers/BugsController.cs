8794160fee0f460799ebaed005a3ba45b1780b9b
Mahesh Singh Madai <maheshmadai977@gmail.com>
2023-05-02 17:12:52 +0545

    Error-handling

---
+++ WebAPI/Controllers/BugsController.cs
@@ -0,0 +1,55 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Errors;
+
+namespace WebAPI.Controllers
+{
+    public class BugsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BugsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("testauth")]
+        [Authorize]
+        public ActionResult<string> GetSecretText()
+        {
+            return "secret stuff";
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(42);
+
+            if (thing == null) return NotFound(new ApiResponse(404));
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
